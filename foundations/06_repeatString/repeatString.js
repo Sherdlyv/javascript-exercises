7b6aa79731436c0ec157380eff295a395ecb889d
Sherly2000 <sherdlyvr@gmail.com>
2026-06-09 23:19:35 -0400

    Complete exercise repeateString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+if(num<0){
+    return 'ERROR';
+}
+for(let i=0; i>num; i++){
+    return string;
+}
+return string.repeat(3);
 };
+console.log(repeatString('hey'));
 
 // Do not edit below this line
 module.exports = repeatString;
