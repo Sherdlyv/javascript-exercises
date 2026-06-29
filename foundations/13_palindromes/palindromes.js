edea40f6bac11655eb971f4833e52069cfc9e371
Sherly2000 <sherdlyvr@gmail.com>
2026-06-28 23:00:44 -0400

    Complete the exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (a) {
+
+   let lowerCaseWord = a.toLowerCase();
+
+   let cleanedWord = lowerCaseWord.replace(/[^a-z0-9]/g, "");
+
+let reversedWord = cleanedWord.split("").reverse().join("");
+
+
+if(reversedWord === cleanedWord ){
+    return true;
+}
+else if(reversedWord === 1){
+    return false;
+}
+
+else{
+    return false;
+}
 
 };
 
