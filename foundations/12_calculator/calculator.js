4b6e3eb0b7758fe69e2c5ef844317c9c672a36cd
Sherly2000 <sherdlyvr@gmail.com>
2026-06-28 22:17:05 -0400

    Complete the exercice 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+
+	let total= arr.reduce((accumulator, item )=>{
+    return accumulator+item;
+ 
+  }, 0);
+
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+let total= arr.reduce((accumulator, item )=>
+  {
+    return accumulator*item;
+  },1);
+  return total;
+};
+
+const power = function(a,b) {
+	return a ** b;
 
 };
 
-const power = function() {
-	
-};
+const factorial = function(a) {
+  let result =1;
 
-const factorial = function() {
-	
+	for(let i=1; i<=a; i++){
+    result = result *i;
+  }
+  return result;
+
 };
 
 // Do not edit below this line
