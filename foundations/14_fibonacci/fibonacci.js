c16758cb6ad1af20ccaf5e7f72a72db7839ac009
Sherly2000 <sherdlyvr@gmail.com>
2026-06-29 22:17:16 -0400

    Complete the exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+  let n= Number(count);
 
+  if (n < 0) {
+        return "OOPS";
+    }
+
+  if (n === 0) {
+        return 0;
+    }
+
+    let fib =[0, 1];
+
+     for (let i = 2; i <= n; i++) {
+         let suivant = fib[i - 1] + fib[i - 2];
+        fib.push(suivant); 
+    }
+
+
+     return fib[n];
 };
 
 // Do not edit below this line
