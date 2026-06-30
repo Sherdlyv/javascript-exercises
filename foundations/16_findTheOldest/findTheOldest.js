4f4875a8b3b871b35e6f0bfb14d4551f515f5c0d
Sherly2000 <sherdlyvr@gmail.com>
2026-06-29 23:12:08 -0400

    Complete exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    let currentYear = new Date().getFullYear();
+
+    let oldest = arr.sort((a,b)=>{
+
+        let deathA = a.yearOfDeath ? a.yearOfDeath : currentYear;
+        let deathB = b.yearOfDeath ? b.yearOfDeath : currentYear;
+
+
+        let ageA = deathA - a.yearOfBirth;
+        let ageB = deathB - b.yearOfBirth;
+
+     return ageB - ageA;
+
+    });
+
+  return oldest[0];
 };
 
 // Do not edit below this line
